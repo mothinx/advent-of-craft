ad4d993e3f2656d4e6808747ccd11df73e8ec086
Julien Gracia <gracia.julien@gmail.com>
2023-12-03 10:05:50 +0100

    refacto: Get rid of indentations
    - Extract methods
    - Get rid of magic numbers / strings

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -1,27 +1,39 @@
 package games;
 
 public class FizzBuzz {
+    public static final int MIN_RANGE = 0;
+    public static final int MAX_RANGE = 100;
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final int BUZZ_MULTIPLE = 5;
+    public static final int FIZZ_MULTIPLE = 3;
+
     private FizzBuzz() {
     }
-    
+
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
-            }
-        } else {
-            throw new OutOfRangeException();
+        if (isOutOfRange(input)) throw new OutOfRangeException();
+        if (isFizz(input) && isBuzz(input)) {
+            return FIZZ + BUZZ;
         }
+        if (isFizz(input)) {
+            return FIZZ;
+        }
+        if (isBuzz(input)) {
+            return BUZZ;
+        }
+        return input.toString();
+    }
+
+    private static boolean isOutOfRange(Integer input) {
+        return input <= MIN_RANGE || input > MAX_RANGE;
+    }
+
+    private static boolean isBuzz(Integer input) {
+        return input % BUZZ_MULTIPLE == 0;
+    }
+
+    private static boolean isFizz(Integer input) {
+        return input % FIZZ_MULTIPLE == 0;
     }
 }
